652a2eb309051a69179f1ebd46b8fcb79426e53d
paresharma <paresharma@gmail.com>
2015-03-24 17:50:33 +0100

    Errors

---
+++ lib/scorm_rte/errors.rb
@@ -0,0 +1,11 @@
+module ScormRte
+
+  # = ScormRte
+  #
+  # Generic ScormRte exception class.
+  class ScormRteError < StandardError
+  end
+
+  class NoScoInstanceIdError < ScormRteError
+  end
+end
