b632913593bf048f4702d0aefaba36fa0b77cd65
paresharma <paresharma@gmail.com>
2015-03-24 22:52:19 +0100

    Rails error and attr_reader

--- lib/scorm_rte/lms/sco.rb
+++ lib/scorm_rte/lms/sco.rb
@@ -2,9 +2,13 @@ module ScormRte
   module Lms
     class Sco
       def initialize(sco_instance_id: nil)
-        fail NoScoInstanceIdError, 'Always provide a unique SCO intance ID' unless sco_instance_id
+        unless sco_instance_id
+          fail NoScoInstanceIdError, 'Always provide a unique SCO intance ID'
+        end
         @sco_instance_id = sco_instance_id
       end
+
+      attr_reader :sco_instance_id
     end
   end
 end
